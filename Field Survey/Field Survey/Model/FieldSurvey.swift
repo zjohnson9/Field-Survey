cb3abc460a5e8d7f606f532c9c175f25343b8bff
Zac Johnson <zejc5f@mail.missouri.edu>
2018-11-14 15:49:27 -0600

    Applied changes to FieldSurvey

--- Field Survey/Field Survey/Model/FieldSurvey.swift
+++ Field Survey/Field Survey/Model/FieldSurvey.swift
@@ -9,19 +9,20 @@
 import Foundation
 
 struct FieldSurvey {
-    let classification = Classification
+    let classification = String
     let title: String
     let description: String
     let date: Date
     
-    init(title: String, description: String, date: Date) {
+    init(classification: String, title: String, description: String, date: Date) {
+        self.classification = classification
         self.title = title
         self.description = description
         self.date = date
     }
     
-    init?(classification: Classification, title: String, description: String, date: Date) {
-        if let classification = Classification(rawValue: title) {
+    init?(classification: String, title: String, description: String, date: Date) {
+        if let classification = classification(rawValue: classification) {
             self.init(title: title, description: description, date: date)
         }
         else {
--- Field Survey/Field Survey/Model/FieldSurveyJSONParser.swift
+++ Field Survey/Field Survey/Model/FieldSurveyJSONParser.swift
@@ -9,13 +9,33 @@
 import Foundation
 
 class FieldSurveyJSONParser {
+    
+    static let dateFormatter = DateFormatter()
+    
     class func parse(_ data: Data) -> [FieldSurvey] {
         var fieldSurvey = [FieldSurvey]()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
         
         if let json = try? JSONSerialization.jsonObject(with: data, options: []),
-        let root = json as? [String: Any]
-            {
+            let root = json as? [String: Any],
+            let status = root["status"] as? String,
+            status == "ok" {
             
+            if let observations = root["observations"] as? [Any] {
+                for observation in observations {
+                    if let observation = observation as? [String: String] {
+                        if let classification = observation["classification"],
+                        if let title = observation["title"],
+                        if let description = observation["description"],
+                        if let date = observation["date"],
+                        let date = dateFormatter.date(from: dateString) {
+                            if let  = fieldSurvey = FieldSurvey(classification: classification, title: title, description: description, date: date) {
+                                fieldSurvey.append(FieldSurvey)
+                            }
+                        }
+                    }
+                }
+            }
         }
         
         
